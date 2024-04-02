3c7a0dc1d33cfd028e31a68862bc7e6ea9cf149e
smenasezona <artemkiasko@gmail.com>
2024-04-02 11:55:40 +0300

    add logs

--- task_1/app.js
+++ task_1/app.js
@@ -2,3 +2,6 @@ const currentDate = () =>
 	new Date().toISOString().slice(0, 10).replace(/-/g, '/')
 
 const currentTime = () => new Date().toLocaleTimeString()
+
+console.log(currentDate())
+console.log(currentTime())
